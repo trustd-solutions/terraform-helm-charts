a97852d5beb56b5b7dadf6b5a07b22653d242774
Bruno Amaral F <brunoamaral@gmail.com>
2021-10-11 17:03:01 -0300

    Edit release_status output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "release_status" {
   description = "Block status of the deployed release"
-  value       = helm_release.this.metadata
+  value       = helm_release.this[each.key].metadata
 }
